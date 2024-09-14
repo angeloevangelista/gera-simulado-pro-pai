ff5cab6fa84a25636fda18814b123ed944fcc5f5
Angelo Evangelista <angeloevan.ane@gmail.com>
2024-09-14 00:32:39 -0300

    fix: remove unused vars, apparently my trashy code needs to be concise

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 import questionsJson from "./assets/questions.json";
